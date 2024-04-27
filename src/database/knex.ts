c4b8575abc5ff802baf3f78cea1a2884028e96b0
Giovanni de Souza Martins <86680890+GSMartins999@users.noreply.github.com>
2024-04-26 23:59:30 -0300

    Update knex.ts

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -3,7 +3,7 @@ import { knex } from "knex"
 export const db = knex({
     client: "sqlite3",
     connection: {
-        filename: "./src/database/database.db", 
+        filename: "./database.db", 
     },
     useNullAsDefault: true,
     pool: {
